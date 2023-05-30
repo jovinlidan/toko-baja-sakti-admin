1b9fd4101fbc3b1e67a991a58121aca2b1d68165
jovinlidan <jovinlidan1@gmail.com>
2023-05-30 16:53:10 +0700

    delete fitur hapus pembelian

--- src/modules/purchase/view.tsx
+++ src/modules/purchase/view.tsx
@@ -7,16 +7,12 @@ import { styled } from "@/config/stitches/theme.stitches";
 import routeConstant from "@/constants/route.constant";
 import useDialog from "@/hooks/use-dialog";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
-import { toast } from "react-hot-toast";
-import { useDeletePurchase } from "@/api-hooks/purchase/purchase.mutation";
 import { useGetPurchase } from "@/api-hooks/purchase/purchase.query";
 import PurchaseForm from "./components/form";
 
 export default function ViewPurchase() {
   const router = useRouter();
   const dialog = useDialog();
-  const { mutateAsync } = useDeletePurchase();
 
   const { data, isLoading, isFetching, error, refetch } = useGetPurchase(
     {
@@ -24,25 +20,6 @@ export default function ViewPurchase() {
     },
     { enabled: !!router?.query?.id }
   );
-
-  const handleDelete = useCallback(() => {
-    dialog.showConfirmation({
-      title: "Hapus Pembelian",
-      message: "Apakah anda yakin? Aksi tidak dapat dibatalkan",
-      onPositiveAction: async (close) => {
-        try {
-          const { message } = await mutateAsync({
-            id: router?.query?.id as string,
-          });
-          message && toast.success(message);
-          router.replace(routeConstant.PurchaseList);
-          close();
-        } catch (e: any) {
-          e?.messsage && toast.error(e?.message);
-        }
-      },
-    });
-  }, [dialog, mutateAsync, router]);
 
   return (
     <Container>
@@ -73,10 +50,6 @@ export default function ViewPurchase() {
                 }}
               >
                 UBAH
-              </Button>
-              <Separator mr={24} />
-              <Button variant="error" onClick={handleDelete}>
-                HAPUS
               </Button>
             </Row>
           </>
